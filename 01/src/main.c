92a59b310ef810c538598aad04a02184871ce938
jc3wrld999 <jc3wrld999@gmail.com>
2025-09-07 18:34:36 +0900

    Rendering an SDL window

--- 01/src/main.c
+++ 01/src/main.c
@@ -2,7 +2,9 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
+bool is_running = false;
 SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
 
 bool init() {
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
@@ -10,16 +12,62 @@ bool init() {
         return false;
     }
     window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        return false;
+    }
+    // Create renderer for window
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL) {
+        printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
+        return false;
+    }
     return true;
 }
 
-int main(void) {
-    if (!init()) {
-        printf("Failed to initialize!\n");
-        return -1;
+void setup(void) {
+    // Placeholder for setup code
+}
+
+void process_input(void) {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+
+    switch(event.type) {
+        case SDL_QUIT:
+            is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                is_running = false;
+            }
+            break;
+        default:
+            break;
     }
-    SDL_Delay(3000);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+}
+
+void update(void) {
+    // Placeholder for update logic
+}
+
+void render(void) {
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+}
+
+int main(void) {
+    is_running = init();
+
+    setup();
+
+    while(is_running) {
+        process_input();
+        update();
+        render();
+        SDL_Delay(16); // Roughly 60 FPS
+    }
+
 	return 0;
 }
